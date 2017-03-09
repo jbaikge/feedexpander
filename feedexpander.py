d339a3cd402ba2331fa82ec3aafe9a30167e6c48
jake <bigjake2s@gmail.com>
2017-03-08 23:18:38 -0500

    Still regurgitating original feed data to ATOM on the output

--- feedexpander.py
+++ feedexpander.py
@@ -11,24 +11,36 @@ feed_in = ''
 for line in fileinput.input():
     feed_in += line
 
-feed = feedparser.parse(feed_in)
+doc = feedparser.parse(feed_in)
 
-root = etree.Element('feed', version='1.0', xmlns='http://www.w3.org/2005/Atom')
-# etree.SubElement(rss, 'title').text = feed.title
+feed = etree.Element('feed', xmlns='http://www.w3.org/2005/Atom')
+etree.SubElement(feed, 'id').text = doc.feed.id
+etree.SubElement(feed, 'title').text = doc.feed.title
+etree.SubElement(feed, 'subtitle').text = doc.feed.subtitle
+etree.SubElement(feed, 'updated').text = doc.feed.updated
 
-for e in feed.entries:
+author = etree.SubElement(feed, 'author')
+for k in doc.feed.author_detail:
+    etree.SubElement(author, k).text = doc.feed.author_detail[k]
+
+for e in doc.entries:
     article = newspaper.Article(e.link)
-    article.download()
-    article.parse()
-    entry = etree.SubElement(root, 'entry')
+    # article.download()
+    # article.parse()
+    entry = etree.SubElement(feed, 'entry')
+    etree.SubElement(entry, 'id').text = e.id
     etree.SubElement(entry, 'link').text = e.link
-    etree.SubElement(entry, 'author').text = e.author
-    etree.SubElement(entry, 'description').text = article.text
-    break
+    etree.SubElement(entry, 'updated').text = e.updated
+    etree.SubElement(entry, 'summary').text = e.summary
+
+    author = etree.SubElement(entry, 'author')
+    for k in e.author_detail:
+        etree.SubElement(author, k).text = e.author_detail[k]
+    # etree.SubElement(entry, 'content').text = article.text
 
 xml = etree.tostring(
-    root,
-    encoding='UTF-8',
+    feed,
+    encoding=doc.encoding,
     pretty_print=True,
     xml_declaration=True)
 
